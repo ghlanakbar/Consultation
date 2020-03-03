217a411eb6d84d6f1a2010d1401c184f15ce6e56
ghlanakbar <50832482+ghlanakbar@users.noreply.github.com>
2020-03-03 15:50:14 +0000

    Update urls.py

--- MyApp/MyApp/urls.py
+++ MyApp/MyApp/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
+#from django.urls import path
+from django.conf.urls import url
+from ConsultationApp.views import Consultation,Register
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
+    url(r'^consultation/$',Consultation,name='consultation'),
+    url(r'^register/$',Register,name='register'),
 ]
