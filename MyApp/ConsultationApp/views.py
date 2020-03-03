6c9dfc5a474d92a4e1581f8099189311ca0dd42f
ghlanakbar <50832482+ghlanakbar@users.noreply.github.com>
2020-03-03 15:43:00 +0000

    view

--- MyApp/ConsultationApp/views.py
+++ MyApp/ConsultationApp/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from ConsultationApp import models
+from ConsultationApp import forms
+
+
+
+def Consultation(request):
+	consultations=models.Consultation.objects.all()
+	context={
+	'consultations':consultations
+	}
+	return render(request,'consultations.html',context)
+
+def Register(request):
+	form_data=forms.UserRegistrar(request.POST or None)
+	msg=''
+	if form_data.is_valid():
+		consultation=models.Consultation()
+		consultation.nom=form_data.cleaned_data['nom']
+		consultation.prenom=form_data.cleaned_data['prenom']
+		consultation.date_naissance=form_data.cleaned_data['date_naissance']
+		consultation.photo=form_data.cleaned_data['photo']
+		consultation.sexe=form_data.cleaned_data['sexe']
+		consultation.groupe_sanguin=form_data.cleaned_data['groupe_sanguin']
+		consultation.poids=form_data.cleaned_data['poids']
+		consultation.taille=form_data.cleaned_data['taille']
+		consultation.observations=form_data.cleaned_data['observations']
+		consultation.save()
+		msg='data is saved'
+
+	context={
+		'formregister':form_data,
+		'msg':msg
+		}
+	return render(request,'register.html',context)
