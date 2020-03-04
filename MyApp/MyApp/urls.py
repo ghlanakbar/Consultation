2aa233d69fb5620fe40f7d2729c93d72241361ea
ghlanakbar <50832482+ghlanakbar@users.noreply.github.com>
2020-03-04 10:39:27 +0000

    Update urls.py

--- MyApp/MyApp/urls.py
+++ MyApp/MyApp/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-#les urls du views
+#les urls des views
 from django.contrib import admin
 #from django.urls import path
 from django.conf.urls import url
