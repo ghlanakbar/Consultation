288fe1bc11a7a88631c8b0abcbe9e36330aba00e
ghlanakbar <50832482+ghlanakbar@users.noreply.github.com>
2020-03-04 10:10:15 +0000

    Update admin.py

--- MyApp/ConsultationApp/admin.py
+++ MyApp/ConsultationApp/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from ConsultationApp.models import Consultation
+from ConsultationApp.models import Consultation  #Configuiration du Table Consultation dans la base de donnée
 
 admin.site.register(Consultation)
